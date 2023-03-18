c0e4bfe1c2061b45c5ef945ff9e23f69c982fe41
rzkjanuarr <rzkjanuarr@gmail.com>
2023-03-18 12:09:51 +0700

    statitistic / expences

--- components/ContentStatistic.tsx
+++ components/ContentStatistic.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./header/Header";
 import Income from "./income/Income";
+import Expences from "./expences/Expences";
 
 function ContentStatistic() {
   return (
     <div className="flex-col flex-none mt-8 mb-4 mx-7 w-80">
       <Header />
       <Income />
+      <Expences />
     </div>
   );
 }
--- components/expences/Expences.tsx
+++ components/expences/Expences.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Income() {
+function Expences() {
   return (
     <div className="h-[190px] w-[320px] rounded-md bg-[#fefefe] shadow-lg transition duration-300 hover:cursor-pointer hover:bg-primary hover:text-white hover:shadow-lg">
       <div className="px-6 pt-6 pb-5 space-y-3">
         <div className="flex flex-row items-center justify-between">
-          <h1 className="text-gray-01">Income</h1>
+          <h1 className="text-gray-01">Expences</h1>
           <span className="text-base text-gray-03">Today</span>
         </div>
         <hr
@@ -16,21 +16,21 @@ function Income() {
           className="my-1"
         />
         <div className="flex flex-row items-center justify-between">
-          <h1 className="text-2xl font-bold">$ 9460.00</h1>
-          <span className="text-xl text-red-500">↓ 1.5%</span>
+          <h1 className="text-2xl font-bold">$ 5660.00</h1>
+          <span className="text-xl text-green">↑ 2.5%</span>
         </div>
         <div>
           <span className="text-sm text-gray-02">
-            Compared to $9940 yesterday
+            Compared to $5240 yesterday
           </span>
         </div>
         <div className="flex items-center justify-between -space-x-2 text-sm text-gray-01">
           <h1>Last week Income</h1>
-          <span>$25658.00</span>
+          <span>$22658.00</span>
         </div>
       </div>
     </div>
   );
 }
 
-export default Income;
+export default Expences;
